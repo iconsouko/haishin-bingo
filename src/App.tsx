64e08dc78d3796b7812df8067bef64ca613a902b
iconsouko <iconsoukoadmin@gmail.com>
2026-09-11 13:57:48 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { BingoChecker } from './components/BingoChecker'
 
 type Step = 0 | 1 | 2 | 3
 
-const DEFAULT_LAYOUT: CardLayout = { xPct: 0.25, yPct: 0.15, sizePct: 0.5 }
+const DEFAULT_LAYOUT: CardLayout = { xPct: 0.1, yPct: 0.165, sizePct: 0.8 }
 const DEFAULT_BG_TRANSFORM: BackgroundTransform = { offsetX: 0, offsetY: 0, scale: 1 }
 
 export default function App() {
